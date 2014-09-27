6636a664557fcd353ead3cff45e13f86bfd802b0
markr_000 <markr_000@Jet>
2014-09-27 16:02:13 -0500

    Added format for console output of receipt in Receipt class.  Decimal and number formatting not working with my doubles for some unknown reason.

--- src/pointofsale/Receipt.java
+++ src/pointofsale/Receipt.java
@@ -5,6 +5,11 @@
  */
 package pointofsale;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  *
  * @author mdeboer1
@@ -13,6 +18,16 @@ public class Receipt {
     //Static variable to count number of objects created, to be used for receiptNumber property
     public static int receiptNumberIncrementer = 1;
     private final int receiptNumber;
+    private String storeName = "Kohl's Department Store";
+    private String thankYou = "\n\t\tThank you for shopping at Kohl's!";
+    private double extendedPrice;
+    private double amountSavedPerLineItem;
+    private double grandTotalOfDiscount;
+    private double subtotal;
+    private double taxes = .051;
+    private double taxAmount;
+    private double grandTotal;
+    
     private LineItem [] lineItems = new LineItem[1];
     
     public Receipt(){
@@ -35,13 +50,54 @@ public class Receipt {
     }
     
     public String getReceipt(){
-        String temp = "";
+//        NumberFormat nf = NumberFormat.getCurrencyInstance();
+//        DecimalFormat decimalFormat = new DecimalFormat("#0.00");
+        Calendar currentDate = Calendar.getInstance(); //Get the current date
+        SimpleDateFormat formatter= new SimpleDateFormat("MMM dd yyyy HH:mm:ss"); //format it as per your requirement
+        String getTodaysDate = formatter.format(currentDate.getTime());
+        String receiptHeader = storeName + "\n" + "Date: " + getTodaysDate + 
+                "\nReceipt Number: " + receiptNumber;
+        String firstLineItem = "";
+        String nextLineItem = " ";
         for (int i = 0; i <= lineItems.length - 1; i++){
-             temp = lineItems[i].getCustomerNumber() + " " +
-                    lineItems[i].getQuantity() + " " + lineItems[i].getProductNumber() +
-                    " " + lineItems[i].getProductDescription() + " " + lineItems[i].getPrice() + 
-                    " " + lineItems[i].getDiscountAmount();
+            if (i == 0){
+                if (lineItems[1].getDiscountAmount() < 1){
+                    amountSavedPerLineItem = (lineItems[i].getDiscountAmount() * 
+                            (lineItems[i].getPrice() * lineItems[i].getQuantity()));
+                }
+                else {
+                    amountSavedPerLineItem = (lineItems[i].getDiscountAmount() * 
+                            lineItems[i].getQuantity());
+                }
+                grandTotalOfDiscount += amountSavedPerLineItem;
+                extendedPrice = (lineItems[i].getPrice() * lineItems[i].getQuantity()) 
+                        - amountSavedPerLineItem;
+                subtotal += extendedPrice;
+                firstLineItem = "\nCustomer Number: " + lineItems[i].getCustomerNumber() +
+                        "\n---------------------------------------------------------------\n" +
+                        lineItems[i].getProductNumber() + "\t" + lineItems[i].getProductDescription() + 
+                        "\t" + lineItems[i].getQuantity() + "\t" + lineItems[i].getPrice() + 
+                        "\t" + amountSavedPerLineItem + "\t" + extendedPrice + "\n";
+            }
+            if (i > 0 && (!(i > lineItems.length - 1))){
+                amountSavedPerLineItem = (lineItems[i].getDiscountAmount() * 
+                        (lineItems[i].getPrice() * lineItems[i].getQuantity()));
+                grandTotalOfDiscount += amountSavedPerLineItem;
+                extendedPrice = (lineItems[i].getPrice() * lineItems[i].getQuantity()) 
+                        - amountSavedPerLineItem;
+                subtotal += extendedPrice;
+                nextLineItem = lineItems[i].getProductNumber() + "\t" + lineItems[i].getProductDescription() 
+                    + "\t" + lineItems[i].getQuantity() + "\t" + lineItems[i].getPrice() + 
+                    "\t" + amountSavedPerLineItem + "\t" + extendedPrice + "\n";
+            }
         }
-        return temp;
+        taxAmount = subtotal * taxes;
+        grandTotal = subtotal + taxAmount;
+        String receiptTotals = "\n\t\t\t\t\tTotal Saved: " + grandTotalOfDiscount +
+                "\n\t\t\t\t\tSubtotal: " + subtotal + "\n" + "\t\t\t\t\tTaxes: " + 
+                taxAmount + "\n\t\t\t\t\tGrand Total: " + grandTotal + "\n";        
+                
+        return receiptHeader + firstLineItem + nextLineItem + receiptTotals + thankYou;
+        
     }
 }
--- src/pointofsale/Startup.java
+++ src/pointofsale/Startup.java
@@ -13,9 +13,14 @@ public class Startup {
     public static void main(String[] args) {
         POSRegister register = new POSRegister();
         register.setFirstLineItem(1, "00004", "3");
-        System.out.println(register.getReceipt());
         register.setNextLineItem(1, "00002", "3");
         System.out.println(register.getReceipt());
+        
+//        POSRegister register2 = new POSRegister();
+//        register2.setFirstLineItem(1, "00001", "2");
+//        register2.setNextLineItem(1, "00002", "2");
+//        register2.setNextLineItem(1, "00004", "2");
+//        System.out.println(register2.getReceipt());
         
     }
     
