a8f9a09a350b47c8de6e76221695eaa43a5e488e
mdeboer1 <mdeboer1@L15203039988.wctc.msft>
2014-10-01 16:21:44 -0500

    Commented out code in Receipt class that has now been moved to the lower level receipt generator class.

--- src/pointofsale/Receipt.java
+++ src/pointofsale/Receipt.java
@@ -5,8 +5,8 @@
  */
 package pointofsale;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+//import java.text.SimpleDateFormat;
+//import java.util.Calendar;
 
 /**
  *This class creates the receipt.  It uses a custom constructor to create the
@@ -26,11 +26,11 @@ public class Receipt {
     private final int receiptNumber;
     private String storeName = "Kohl's Department Store";
     private String thankYou = "\n\t\tThank you for shopping at Kohl's!\n\n";
-    private double grandTotalOfDiscount;
-    private double subtotal;
-    private double taxes = .051;
-    private double taxAmount;
-    private double grandTotal;
+//    private double grandTotalOfDiscount;
+//    private double subtotal;
+//    private double taxes = .051;
+//    private double taxAmount;
+//    private double grandTotal;
     private String customerNumber;
     private LineItem [] lineItems = new LineItem[1];
     private FakeDataBase database;
@@ -89,39 +89,39 @@ public class Receipt {
         lineItems = tempArray;
     }
     
-    /**
-     * Returns a fully formatted console receipt.
-     * 
-     */
-    public final void getConsoleReceipt(){
-        Calendar currentDate = Calendar.getInstance(); //Get the current date
-        SimpleDateFormat formatter= new SimpleDateFormat("MMM dd yyyy HH:mm:ss"); //format it as per your requirement
-        String getTodaysDate = formatter.format(currentDate.getTime());
-        System.out.println(storeName + "\n" + "Date: " + getTodaysDate + 
-                "\nReceipt Number: " + receiptNumber);
-        for (int i = 0; i <= lineItems.length - 1; i++){
-            if (i == 0){
-                grandTotalOfDiscount += lineItems[i].getAmountSavedPerLine();
-                subtotal += lineItems[i].getExtendedPrice();
-                System.out.println("Customer Number: " + customerNumber +
-                        "\n---------------------------------------------------------------\n");
-                System.out.printf("%s \t %s \t %d \t %.2f \t %.2f \t %.2f \n", lineItems[i].getProductNumber(),
-                        lineItems[i].getProductDescription(), lineItems[i].getQuantity(),
-                        lineItems[i].getPrice(), lineItems[i].getAmountSavedPerLine(), lineItems[i].getExtendedPrice());
-            }
-            if (i > 0 && (!(i > lineItems.length - 1))){
-                grandTotalOfDiscount += lineItems[i].getAmountSavedPerLine();
-                subtotal += lineItems[i].getExtendedPrice();
-                System.out.printf("%s \t %s \t %d \t %.2f \t %.2f \t %.2f \n", lineItems[i].getProductNumber(),
-                        lineItems[i].getProductDescription(), lineItems[i].getQuantity(),
-                        lineItems[i].getPrice(), lineItems[i].getAmountSavedPerLine(), lineItems[i].getExtendedPrice());
-            }
-        }
-        taxAmount = subtotal * taxes;
-        grandTotal = subtotal + taxAmount;
-        System.out.printf("\n\t\t\t\tTotal Saved: \t %.2f \n\t\t\t\tSubtotal: \t%.2f" +
-          "\n\t\t\t\tTaxes: \t\t %.2f \n\t\t\t\tGrand Total: \t%.2f\n%s",
-                grandTotalOfDiscount,subtotal,taxes,grandTotal, thankYou);
-        
-    }
+//    /**
+//     * Returns a fully formatted console receipt.
+//     * 
+//     */
+//    public final void getConsoleReceipt(){
+//        Calendar currentDate = Calendar.getInstance(); //Get the current date
+//        SimpleDateFormat formatter= new SimpleDateFormat("MMM dd yyyy HH:mm:ss"); //format it as per your requirement
+//        String getTodaysDate = formatter.format(currentDate.getTime());
+//        System.out.println(storeName + "\n" + "Date: " + getTodaysDate + 
+//                "\nReceipt Number: " + receiptNumber);
+//        for (int i = 0; i <= lineItems.length - 1; i++){
+//            if (i == 0){
+//                grandTotalOfDiscount += lineItems[i].getAmountSavedPerLine();
+//                subtotal += lineItems[i].getExtendedPrice();
+//                System.out.println("Customer Number: " + customerNumber +
+//                        "\n---------------------------------------------------------------\n");
+//                System.out.printf("%s \t %s \t %d \t %.2f \t %.2f \t %.2f \n", lineItems[i].getProductNumber(),
+//                        lineItems[i].getProductDescription(), lineItems[i].getQuantity(),
+//                        lineItems[i].getPrice(), lineItems[i].getAmountSavedPerLine(), lineItems[i].getExtendedPrice());
+//            }
+//            if (i > 0 && (!(i > lineItems.length - 1))){
+//                grandTotalOfDiscount += lineItems[i].getAmountSavedPerLine();
+//                subtotal += lineItems[i].getExtendedPrice();
+//                System.out.printf("%s \t %s \t %d \t %.2f \t %.2f \t %.2f \n", lineItems[i].getProductNumber(),
+//                        lineItems[i].getProductDescription(), lineItems[i].getQuantity(),
+//                        lineItems[i].getPrice(), lineItems[i].getAmountSavedPerLine(), lineItems[i].getExtendedPrice());
+//            }
+//        }
+//        taxAmount = subtotal * taxes;
+//        grandTotal = subtotal + taxAmount;
+//        System.out.printf("\n\t\t\t\tTotal Saved: \t %.2f \n\t\t\t\tSubtotal: \t%.2f" +
+//          "\n\t\t\t\tTaxes: \t\t %.2f \n\t\t\t\tGrand Total: \t%.2f\n%s",
+//                grandTotalOfDiscount,subtotal,taxes,grandTotal, thankYou);
+//        
+//    }
 }
